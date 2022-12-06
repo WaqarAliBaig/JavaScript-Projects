c0ac3f02ac89bb42e708e33b4ba408d2ad367765
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-06 16:34:51 +0500

    Create coding.js

---
+++ 9. Favorite Number/coding.js
@@ -0,0 +1,6 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favNumber = 2;
+let msg = favNumber + " is my favorite number.";
+
+console.log(msg);

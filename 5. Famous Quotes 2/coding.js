40fdde9845335b9a1145bf7ddbfa79f10b30cf80
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-06 16:00:15 +0500

    Create coding.js

---
+++ 5. Famous Quotes 2/coding.js
@@ -0,0 +1,7 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let quote = "None of you [truly] BELIEVES until he wishes for his BROTHER what he wishes for HIMSELF.";
+let famous_person = "Prophet Muhammad (S.A.W)";
+let message = famous_person + `:` + " Once said, " + `"` + quote + `"`;
+
+console.log(message);

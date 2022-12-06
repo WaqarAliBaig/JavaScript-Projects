2933b0bca3c172aebfdba3af6bef5390226d3afe
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-06 16:27:23 +0500

    Create coding.js

---
+++ 7-8. Number Eight/coding.js
@@ -0,0 +1,12 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+let lines = `________________________________`;
+console.log(lines);
+console.log(`5 + 3 = ` + (5+3)); 
+console.log(lines);
+console.log(`12 - 4 = ` + (12-4)); 
+console.log(lines);
+console.log(`4 * 2 = ` + (4*2)); 
+console.log(lines);
+console.log(`80 / 10 = ` + (80/10)); 
+console.log(lines);

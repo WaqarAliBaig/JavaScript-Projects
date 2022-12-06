a4cc543c338fe625fa8f3051ad08ed9851ae36ab
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-06 16:01:11 +0500

    Update coding.js

--- 4. Famous Quotes/coding.js
+++ 4. Famous Quotes/coding.js
@@ -1,6 +1,6 @@
 //Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 
-let quote = "None of you [truly] BELIEVES until he wishes for his BROTHER what he wishes for HIMSELF"
-let author = "Prophet Muhammad (S.A.W)"
+let quote = "None of you [truly] BELIEVES until he wishes for his BROTHER what he wishes for HIMSELF.";
+let author = "Prophet Muhammad (S.A.W)";
 
 console.log(author + `:` + " Once said, " + `"` + quote + `"`);

208308f93c80f2e0115c0cc57098193db86a6d33
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-07 15:16:33 +0500

    Create coding.js

---
+++ 11. Names/coding.js
@@ -0,0 +1,12 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let friendsName = ["Aamir Malik", "Amjad Kalim", "Adnan Hanif", "Ammad Siddiqui", "Arsalan Zain", "Muhammad Humayun", "Saad Amin", "Syed Faizan"];
+
+console.log(friendsName[0]);
+console.log(friendsName[1]);
+console.log(friendsName[2]);
+console.log(friendsName[3]);
+console.log(friendsName[4]);
+console.log(friendsName[5]);
+console.log(friendsName[6]);
+console.log(friendsName[7]);

78f7a9e8d386a3ef95af908a15e6c9361ad6e39e
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-07 15:03:14 +0500

    Create coding.js

---
+++ Adding Comments/coding.js
@@ -0,0 +1,37 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// Name:	   Waqar Ali
+// S/O:	     Tahir Ali
+// Roll No:	 PIAIC195474
+// City:	   Karachi
+// Gender:	 Male
+// Email:	   waqaralibaig@hotmail.com
+// Date:     07-12-2022
+
+
+// Program-1
+// Statement-1: Calculation operation that each calculation will result in number 8.
+// Statement-2: Creating lines between every opertaion for sperating the content.
+
+let lines = `________________________________`;
+console.log(lines);
+console.log(`5 + 3 = ` + (5+3)); 
+console.log(lines);
+console.log(`12 - 4 = ` + (12-4)); 
+console.log(lines);
+console.log(`4 * 2 = ` + (4*2)); 
+console.log(lines);
+console.log(`80 / 10 = ` + (80/10)); 
+console.log(lines);
+
+
+// Program-2
+// Statement-1: Creating persons name and adding some whitespace characters in the beginning and end of the name.
+// Statement-2: Triming all the whitespace characters from the persons name.  
+
+let personName = " \t \n \t Waqar Ali \n ";
+
+console.log(personName);
+
+//Print statement without spaces:
+console.log(personName.trim());

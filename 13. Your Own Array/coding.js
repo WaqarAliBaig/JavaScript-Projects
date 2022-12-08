bbcbb3993b734c4b7f176e190c3ab11fad0fa678
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-08 23:49:05 +0500

    Create coding.js

---
+++ 13. Your Own Array/coding.js
@@ -0,0 +1,13 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+// In the Name of ALLAH, the most Beneficial and Merciful.
+// INSHALLAH one day.
+let vehicles = ["Honda Civic X", "Hyundai Sonata", "Haval Jolian HEV", "Audi A7 Sportback", "B.M.W S1000RR 2017", "Kawasaki H2"];
+let colours = ["White", "Black", "Silver", "Glossy Grey or Green", "White colour & Blue stipes with SC Project exhaust system", "Glossy Silver colour with Vandemon full exhaust system"];
+
+console.log("I will own a " + vehicles[0], "in " + colours[0] + " colour one day.");
+console.log("I like to buy a " + vehicles[1], "in " + colours[1] + " colour one day.");
+console.log("I will have a " + vehicles[2], "in " + colours[2] + " colour one day.");
+console.log("I will be the owner of " + vehicles[3], "in " + colours[3] + " colour one day.");
+console.log("I love buy and ride on " + vehicles[4], "fully tuned in " + colours[4] + " one day.");
+console.log("It's my dream to buy a " + vehicles[5], "in " + colours[5] + " one day.");

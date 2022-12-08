7efe4e428adc0e0b036df0bcd4e371fb37c73e20
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-09 00:11:32 +0500

    Create coding.js

---
+++ 14. Guest List/coding.js
@@ -0,0 +1,7 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let guestList = ["Ahad", "Arsalan", "Saad"];
+
+console.log("Hi, " + guestList[0] + " I would like you to have dinner at my home.");
+console.log("Hi, " + guestList[1] + " I am having a get togather for dinner at my home would you like to come?");
+console.log("Hi, " + guestList[2] + " I am inviting you to have dinner with me and all the friends at my home.");

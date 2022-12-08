3485b7705fcc1f6c747dcf3e3e0500ef0d5eb508
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-08 23:48:02 +0500

    Create coding.js

---
+++ 12. Greetings/coding.js
@@ -0,0 +1,11 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let friendsName = ["Aamir", "Amjad", "Adnan", "Ammad", "Arsalan", "Humayun", "Saad", "Faizan"];
+
+console.log("Hey, " + friendsName[0] + " did you play RISE OF THE TOMB RAIDER the game is so adventurous man.");
+console.log("Hey, " + friendsName[1] + " did you play UNCHARTED LEGACY OF THIEVES the game is full of puzzle solving man.");
+console.log("Hey, " + friendsName[2] + " did you play BATMAN ARKHAM KNIGHT the game is ligit man.");
+console.log("Hey, " + friendsName[3] + " did you play DEVIL MAY CRY the game is so action packed man.");
+console.log("Hey, " + friendsName[4] + " did you play CALL OF DUTY MODERN WARFARE the game graphics are awesome man.");
+console.log("Hey, " + friendsName[5] + " did you play BATTLEFIELD V it's a world war story based game man.");
+console.log("Hey, " + friendsName[6] + " did you play NEED FOR SPEED UNDERGROUND the game reminds me of the movie Fast And The Furious man.");

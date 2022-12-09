741364252f0e844044957ac2e61d73a0c8dd1b30
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-09 15:52:29 +0500

    Update coding.js

--- 12. Greetings/coding.js
+++ 12. Greetings/coding.js
@@ -2,10 +2,7 @@
 
 let friendsName = ["Aamir", "Amjad", "Adnan", "Ammad", "Arsalan", "Humayun", "Saad", "Faizan"];
 
-console.log("Hey, " + friendsName[0] + " did you play RISE OF THE TOMB RAIDER the game is so adventurous man.");
-console.log("Hey, " + friendsName[1] + " did you play UNCHARTED LEGACY OF THIEVES the game is full of puzzle solving man.");
-console.log("Hey, " + friendsName[2] + " did you play BATMAN ARKHAM KNIGHT the game is ligit man.");
-console.log("Hey, " + friendsName[3] + " did you play DEVIL MAY CRY the game is so action packed man.");
-console.log("Hey, " + friendsName[4] + " did you play CALL OF DUTY MODERN WARFARE the game graphics are awesome man.");
-console.log("Hey, " + friendsName[5] + " did you play BATTLEFIELD V it's a world war story based game man.");
-console.log("Hey, " + friendsName[6] + " did you play NEED FOR SPEED UNDERGROUND the game reminds me of the movie Fast And The Furious man.");
+for (let i = 0; i < friendsName.length; i++){
+   
+    console.log("Hey, " + friendsName[i] + " how are you and your family?");
+}

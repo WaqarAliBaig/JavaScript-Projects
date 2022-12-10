b67fde4339b17869f53fb5c3aac2f05828c23183
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-10 23:41:55 +0500

    Create coding.js

---
+++ 20. Think of something/coding.js
@@ -0,0 +1,17 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let countries = ["Saudi Arab", "Switzerland", "Japan", "Turkey", "Norway"];
+let cities = ["Mecca", "Zurich", "Tokyo", "Istanbul", "Oslo"];
+let languages = ["Arabic", "Italian", "Japanese", "Turkish", "Norwegian"];
+
+// Countries list:
+console.log("Countries list:");
+console.log(countries);
+
+// Cities list:
+console.log("Cities list:");
+console.log(cities);
+
+// Languages list:
+console.log("Languages list:");
+console.log(languages);

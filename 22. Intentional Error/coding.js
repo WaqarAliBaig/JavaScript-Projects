ed3341660d88d671acc5f1824a3f81bdd67421b1
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-11 01:40:00 +0500

    Create coding.js

---
+++ 22. Intentional Error/coding.js
@@ -0,0 +1,18 @@
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+let countryStatus = [
+    {countryName: "Saudi Arab", city: "Mecca", language: "Arabic"}, 
+    {countryName: "Switzerland", city: "Zurich", language: "Italian"},
+    {countryName: "Japan", city: "Tokyo", language: "Japanese"},
+    {countryName: "Turkey", city: "Istanbul", language: "Turkish"},
+    {countryName: "Norway", city: "Oslo", language: "Norwegian"}
+];
+
+console.log(countryStatus[0]);
+console.log(countryStatus[1]);
+console.log(countryStatus[2]);
+console.log(countryStatus[3]);
+console.log(countryStatus[4]);
+
+// Intentional Error.
+console.log(countrystatus[i]);

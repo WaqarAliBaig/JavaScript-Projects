57aa6f16a8923cef53a3ee8c6869db57bcf77373
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-11 00:35:58 +0500

    Create coding.js

---
+++ 21. They think of something/coding.js
@@ -0,0 +1,15 @@
+//They think of something you could store in a JavaScript Object. Write a program that creates Objects containing these items.
+
+let countryStatus = [
+    {countryName: "Saudi Arab", city: "Mecca", language: "Arabic"}, 
+    {countryName: "Switzerland", city: "Zurich", language: "Italian"},
+    {countryName: "Japan", city: "Tokyo", language: "Japanese"},
+    {countryName: "Turkey", city: "Istanbul", language: "Turkish"},
+    {countryName: "Norway", city: "Oslo", language: "Norwegian"}
+];
+
+console.log(countryStatus[0]);
+console.log(countryStatus[1]);
+console.log(countryStatus[2]);
+console.log(countryStatus[3]);
+console.log(countryStatus[4]);

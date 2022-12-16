0e01091ef6d3eaec6c87b196718be3dce0cc599d
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-16 11:12:38 +0500

    Create coding.js

---
+++ Checking Usernames/coding.js
@@ -0,0 +1,44 @@
+// Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+
+// • Make a list of five or more usernames called current_users.
+
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+
+
+
+// • Make a list of five or more usernames called current_users.
+let current_users = ["Ahad", "Arsalan", "Adnan", "Ammad", "Adil"];
+
+
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+let new_users = ["Ali", "Fuad", "Azeem", "Owais", "Fahad"];
+
+
+// Checking username exist or not:
+for (let i = 0; i < new_users.length; i++) {
+
+    let avl = false;
+
+    for (let j = 0; j < current_users.length; j++) {
+
+        // Username available on current user list: 
+        if (new_users[i].toLowerCase() === current_users[j].toLowerCase()) {
+            console.log(new_users[i] + " please enter a new username");
+
+            avl = false;
+
+            break;
+        }
+        else {
+            avl = true;
+        }
+    }
+    // Username available:
+    if (avl === true) {
+        console.log(new_users[i] + " Username is available");
+    }
+}

b1368481d4e30d2a3f791d0008fcdfd5dd8d8c24
WaqarAliBaig <114983537+WaqarAliBaig@users.noreply.github.com>
2022-12-18 21:06:54 +0500

    Create coding.js

---
+++ 36. T-Shirt/coding.js
@@ -0,0 +1,12 @@
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+
+// Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt.
+
+function make_shirt(size, message) {
+    console.log("Shirt size " + size + "`");
+    console.log("Message printed on shirt " + message + "`");
+}
+
+// Calling function:
+make_shirt("Medium", "Street Fighter");
